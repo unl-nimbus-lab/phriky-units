93c671904a9b9205d7f178c7053bc0f8cac3447e
John-Paul Ore <jore@cse.unl.edu>
2017-09-22 10:37:20 -0700

    version uptick

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ test_requirements = [
 
 setup(
     name='phriky_units',
-    version='0.1.0a31',
+    version='0.1.37',
     description="Physical unit static analysis tool for C++ +",
     long_description=readme + '\n\n' + history,
     author="John-Paul Ore",
